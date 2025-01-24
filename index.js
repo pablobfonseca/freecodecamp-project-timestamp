3f1abc3d6a59113e682740ca75a8d8e7099390fd
Pablo Fonseca <pablofonseca777@gmail.com>
2025-01-24 00:48:30 +0000

    feat: finish

--- index.js
+++ index.js
@@ -2,31 +2,42 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.param("date", (request, response, next, parameter) => {
+  try {
+    if (parameter && parameter.length === 13) {
+      request.date = new Date(parseInt(parameter));
+    } else if (parameter === "") {
+      request.date = new Date();
+    } else {
+      request.date = new Date(parameter);
+    }
+  } catch {
+    response.json({ error: "Invalid date" }).end();
+  }
 });
 
-
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  res.json({ unix: req.date.getTime(), utc: req.date.toUTCString() });
+});
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
